9cfd24638554e1b318358bb94ff91cc9487829e1
FARIX-KILIEN <farixkilien@yahoo.fr>
2022-07-04 08:58:38 -0700

    number in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print numbers
+ *
+ * Description: the numbers are in base 10 and they start from 0
+ *
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i > 0; i++)
+	{
+		pintf("%d\n", i);
+	}
+	return (0);
+}
