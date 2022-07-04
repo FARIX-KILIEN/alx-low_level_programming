6d72ed0b1f151387bdc06b3e5b6ba3a7d64db9df
FARIX-KILIEN <farixkilien@yahoo.fr>
2022-07-04 13:34:11 -0700

    all possible different combinations of two digits.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - all possible different combinations
+ *
+ * Description: all possible different combinations of two digits.
+ *
+ * Return: always success (0)
+ */
+int main(void)
+{
+	int i, e;
+
+	i = 48;
+	e = 48;
+
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			putchar(e);
+			putchar(i);
+			if (i == 57 && e == 57)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
+}
