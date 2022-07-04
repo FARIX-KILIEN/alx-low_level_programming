f411f46edf6bfa98e030565704a2d2158c25d7f3
FARIX-KILIEN <farixkilien@yahoo.fr>
2022-07-04 05:56:02 -0700

    assign a random number to the variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,5 +6,20 @@
  * Returns a pseudo-random integer between 0 and RAND_MAX
  */
 srand(time(NULL));
-
 int r = rand();
+
+n = rand() - RAND_MAX / 2;
+l = n % 10;
+if (l > 5)
+{
+	printf("Last digit of %d is %d and is greater than 5\n", n, l);
+}
+else if (l == 0)
+{
+	printf("Last digit of %d is %d and is 0\n", n, l);
+}
+else
+{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+}
+return (0);
