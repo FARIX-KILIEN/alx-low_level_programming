1a3a78b9a73f874eb7812de79ea25c9288e8c7b1
FARIX-KILIEN <farixkilien@yahoo.fr>
2022-07-04 08:32:11 -0700

    alphabet using the putchar command

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main- write the aphabet
+ *
+ * Description: all the letters should be in lower case
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char alp(26) = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 97; i > 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
