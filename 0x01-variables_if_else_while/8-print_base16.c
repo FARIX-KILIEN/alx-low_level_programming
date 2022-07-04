dc1571a91ac55974ec4ad44ea0d036481f7cf41d
FARIX-KILIEN <farixkilien@yahoo.fr>
2022-07-04 12:32:11 -0700

    all the numbers of base 16 in lowercase,

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include <stdio.hi>
+
  /**
-  * main - Prints numbers between 0 to 9 and letters between a to f.
+  * Main: Prints numbers between 0 to 9 and letters between a to f.
   *
   * Description: all the numbers of base 16 in lowercase
   *
   * Return: always 0 (success)
   */
 int main(void)
-
 {
 	int i = 0;
 	
