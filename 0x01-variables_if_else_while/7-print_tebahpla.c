dc4f5f91e3fdf75b6d7418a73ddc689962d823af
FARIX-KILIEN <farixkilien@yahoo.fr>
2022-07-04 09:54:48 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints reversed alphabet.
+ *
+ * Description: printing the alphabet startint from z to a
+ *
+ * Return : always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
