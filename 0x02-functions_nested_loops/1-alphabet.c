b0e9d13da396253c124610aeb8ba1d6b9991bac6
FARIX-KILIEN <farixkilien@yahoo.fr>
2022-07-05 16:50:13 -0700

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - alphabet in lower case
+ *
+ * Description: printing the alphabet in lower case using the putchar 
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefdhijklmnopqrstuvwxyz";
+	int i
+
+		for (i = 0; i < 26; i++)
+		{
+			putchar(i);
+		}
+	putchar('\n');
+	return (0);
+}
