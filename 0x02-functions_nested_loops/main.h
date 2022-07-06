f1fff0c5ed2a85a65011ae320ae2ddea506e472e
FARIX-KILIEN <farixkilien@yahoo.fr>
2022-07-05 17:15:08 -0700

    lowercase alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include "main.h"
+
 /**
- * main - main file
+ * main - check the code for Holberton School students.
  *
- *
- * 
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	print_alphabet();
+	return (0);
+}
