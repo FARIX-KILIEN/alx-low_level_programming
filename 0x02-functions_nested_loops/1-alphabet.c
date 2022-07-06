68f7cba095ca7f4123202fa64ab401d8332301cb
FARIX-KILIEN <farixkilien@yahoo.fr>
2022-07-05 17:54:35 -0700

    use _putchar to write alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,12 +11,15 @@
  */
 int main(void)
 {
-	int i;
-	
-	for (i = 97; i <= 122; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+
 	return (0);
 }
