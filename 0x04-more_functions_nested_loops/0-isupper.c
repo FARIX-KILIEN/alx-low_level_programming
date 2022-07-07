ac0a6353e3325027a35d52ac45d6b475c5dc0203
FARIX-KILIEN <farixkilien@yahoo.fr>
2022-07-07 15:28:23 -0700

    checking lower and uppercase letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - checks if parameter is an uppercase character.
+ *
+ * Description: uppercase and lowercase checking
+ *
+ * Return: 1 if is an uppercase character, 0 in other case.
+ */
+int main(void)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
