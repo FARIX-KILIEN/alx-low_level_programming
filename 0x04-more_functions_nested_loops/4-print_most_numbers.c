788a1ae56ada56dfd0c168b7add21da4dbc6e0ac
FARIX-KILIEN <farixkilien@yahoo.fr>
2022-07-07 16:58:20 -0700

    prints the numbers, from 0 to 9 wiyhout 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,8 +4,6 @@
  * print_most_numbers - prints numbers between 0 to 9
  *
  * (neither 2 nor 4).
- *
- * Return: no return.
  */
 void print_most_numbers(void);
 {
@@ -13,7 +11,7 @@ void print_most_numbers(void);
 
 	for (X = 0; X < 10; X++)
 	{
-		if (X != 2 && X != 4)
+		if (X !=2 && X !=4)
 			_putchar(X + '0');
 	}
 	_putchar('\n');
