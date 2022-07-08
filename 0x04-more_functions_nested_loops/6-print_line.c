e238da02ebeec3cfcad654325a6be000ee5ab740
FARIX-KILIEN <farixkilien@yahoo.fr>
2022-07-07 19:56:27 -0700

    last commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,11 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_line -> prints straight line n times
  *
  * @n: times straight line is printed.
- *
- * Return: no return.
  */
 void print_line(int n)
 {
