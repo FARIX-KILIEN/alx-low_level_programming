e433ef77a53457c46b310081b4f4d97c739b6bb2
FARIX-KILIEN <farixkilien@yahoo.fr>
2022-07-08 17:40:39 -0700

    9-fizz_buzz.c modified

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,28 +8,28 @@
 int main(void)
 {
 	int n;
-n = 0;
-printf("%d", n);
-for (n = 1; n < 101; n++)
-{
-	if ((n % 3 == 0) && (n % 5 == 0))
+
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n <= 100; n++)
 	{
-		printf(" FizzBuzz");
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", n);
+		}
 	}
-	else if (n % 3 == 0)
-	{
-		printf(" Fizz");
-	}
-	else if (n % 5 == 0)
-		if (n == 100)
-	{
-		printf(" Buzz");
-	}
-	else
-	{
-		printf(" %d", n);
-	}
-}
-printf("\n");
-return (0);
+	printf("\n");
+	return (0);
 }
