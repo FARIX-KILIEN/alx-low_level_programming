3a05dcc8a4ae56c458fe9625e1cbff57dd879211
FARIX-KILIEN <farixkilien@yahoo.fr>
2022-07-08 18:25:12 -0700

    lowercas alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,15 +9,11 @@
  */
 void print_alphabet(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	int i;
 
-	return (0);
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
