320d9342bbd51f5a68bcee67ddf12301df74d524
FARIX-KILIEN <farixkilien@yahoo.fr>
2022-07-18 16:00:00 -0700

    copie memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area,
+ * @dest: destination memory area.
+ * @src: source memory area.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int t;
+
+	for (t = 0; t < n; t++)
+	{
+		*(dest + t) = *(scr + t);
+	}
+	return (dest);
+}
