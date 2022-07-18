b8da5b65999f4c57b01a5cffeca13685cd97a0c4
FARIX-KILIEN <farixkilien@yahoo.fr>
2022-07-18 16:23:15 -0700

    locate character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,10 +11,9 @@ char *_strchr(char *s, char c)
 	unsigned int t = 0;
 
 	for (; *(s + t) != '\0'; t++)
-	{
 		if (*(s + t) == c)
 			return (s + t);
-	}
 	if (*(s + t) == c)
 		return (s + t);
 	return ('\0');
+}
