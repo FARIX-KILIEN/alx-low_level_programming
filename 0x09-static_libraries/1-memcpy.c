d0a6f94fb6595c5f4303f5b7fc9a0f6e0e87b869
FARIX-KILIEN <farixkilien@yahoo.fr>
2022-07-24 16:57:52 -0700

    libraries

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area,
+ * @dest: destination memory area.
+ * @src: source memory area.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int t;
+
+	for (t = 0; t < n; t++)
+	{
+		*(dest + t) = *(src + t);
+	}
+	return (dest);
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - to prints a string
+ * @str: nput string.
+ * Return: no return
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
